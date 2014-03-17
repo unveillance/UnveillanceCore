b73e00a4a6c86ddc4e11b78dbce1b5021c8ccd8c
harlo <harlo.holmes@gmail.com>
2014-03-17 13:33:41 -0400

    with elasticsearch

---
+++ Utils/uv_elasticsearch.py
@@ -0,0 +1,64 @@
+import json
+
+from conf import HOST
+
+class UnveillanceElasticsearch(object):
+	def __init__(self):
+		self.status = False
+	
+	def get(self, _id):
+		print "getting thing"
+		if not self.status: return None
+		
+		res = self.sendELSRequest(endpoint=_id)
+		try:
+			if res['exists']: return res['_source']
+		except KeyError as e: pass
+		
+		return None
+	
+	def query(self, args):
+		print "OH A QUERY"
+	
+	def update(self, _id, args):
+		print "updating thing"
+		if not self.status: return False
+		
+		res = self.sendELSRequest(endpoint=_id, data=args, method="put")
+
+		try: return res['ok']
+		except KeyError as e: pass
+		
+		return False
+	
+	def create(self, _id, args):
+		print "creating thing"
+		return self.update(_id, args)
+	
+	def delete(self, _id):
+		print "deleting thing"
+		if not self.status: return False
+		
+		res = self.sendELSRequest(endpoint=_id, method="delete")
+		
+		try: return res['ok']
+		except KeyError as e: pass
+		
+		return False
+	
+	def sendELSRequest(self, data=None, endpoint=None):
+		url = "http://%s:9200/unveillance/documents/" % HOST
+		if endpoint is not None:
+			url += endpoint
+		
+		if data is not None:
+			data = json.dumps(data)
+		
+		if method == "get":
+			r = requests.get(url, data=data)
+		elif method == "put":
+			r = requests.post(url, data=data)
+		elif method == "delete":
+			r = requests.delete(url, data=data)
+		
+		return json.loads(r.content)
