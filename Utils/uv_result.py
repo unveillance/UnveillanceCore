8ee013a15ecd3f5269bec902da2f65bd338de5fe
harlo <harlo.holmes@gmail.com>
2014-03-31 18:28:36 -0400

    reorg

--- Utils/uv_result.py
+++
@@ -1,5 +0,0 @@
-class Result(object):
-	def __init__(self):
-		self.result = 404
-
-	def emit(self): return self.__dict__
--- vars.py
+++ vars.py
@@ -1,4 +1,10 @@
 from collections import namedtuple
 
 EmitSentinel = namedtuple("EmitSentinel", "attr type s_replace")
-#AnnexDescriptor = namedtuple("AnnexDescriptor", "farm p22 p8888")
+AnnexDescriptor = namedtuple("AnnexDescriptor", "farm p22 p8888")
+
+class Result(object):
+	def __init__(self):
+		self.result = 404
+
+	def emit(self): return self.__dict__
