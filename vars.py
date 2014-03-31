d5fece95da570ffca20ddcf54681c0190ca3d170
harlo <harlo.holmes@gmail.com>
2014-03-31 19:08:37 -0400

    vars

--- vars.py
+++ vars.py
@@ -3,8 +3,11 @@ from collections import namedtuple
 EmitSentinel = namedtuple("EmitSentinel", "attr type s_replace")
 AnnexDescriptor = namedtuple("AnnexDescriptor", "farm p22 p8888")
 
-asset_types = namedtuple("asset_types", "ORIG")
-AssetTypes = asset_types("uv_doc_ORIG")
+mime_types = namedtuple("mime_types", "TEXT EVALUATE")
+MimeTypes = mime_types("plain/txt", ["plain/txt"])
+
+asset_tags = namedtuple("asset_tags", "ORIG F_MD")
+AssetTags = asset_tags("original_document", "metadata_fingerprint")
 
 class Result(object):
 	def __init__(self):
